37e078dafd178a0e9efb2a2e0b92ccb913164b9f
dorra <youssefdorra@gmail.com>
2022-10-20 18:34:50 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/deep_link/deep_link/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.deep_link.deep_link
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/page_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PageScreen extends StatelessWidget {
+  const PageScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Page Screen'),
+    );
+  }
+}
